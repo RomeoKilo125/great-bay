25acacc2fc1a200dc3ad4824a7a99caec11fc92e
mjtedder <mjtedder@gmail.com>
2018-05-26 15:25:05 -0400

    start function complete

--- greatBay.js
+++ greatBay.js
@@ -1,5 +1,6 @@
 require("dotenv").config()
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -7,35 +8,74 @@ var connection = mysql.createConnection({
     user: "root",
     password: process.env.password,
     database: "greatBay_DB"
-})
+});
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
-    console.log("Connected as ID " + connection.threadId)
-    connection.query("SELECT * FROM auctions", function(err, res) {
-      console.log(res)
-      connection.end()
-    })
-  })
-
+    //TESTING CONNECTION
+    //console.log("Connected as ID " + connection.threadId)
+    //connection.query("SELECT * FROM auctions", function (err, res) {
+    //console.log(res)
+    //createProduct();
+    startApp();
+});
 // use inquirer to ask user for post or bid
-
-// once user has decided:
-
+function startApp() {
+    inquirer
+        .prompt({
+            name: "postOrBid",
+            type: "rawlist",
+            message: "Would you like to [POST] an auction or [BID] on an auction?",
+            choices: ["POST", "BID"]
+        })
+        // once user has decided:
+        .then(function(answer) {
     // if posting, create function
-        // inquire for: item name, category, starting bid
+            if (answer.postOrBid.toUpperCase() === "POST") {
+                postAuction();
+            }
+            else {
+                bidAuction();
+            }
+        });
+    }
+    // inquire for: item name, category, starting bid
     //then create function to add item to DB
+    function postAuction() {
+        console.log("Inserting a new bid...\n");
+        var query = connection.query(
+            "INSERT INTO auctions SET ?", {
+                item_name: "Nintendo Switch",
+                category: "Video Games",
+                starting_bid: 0,
+                highest_bid: 0
+            },
+            function (err, res) {
+                console.log(res.affectedRows + " product inserted!\n");
+                readProducts();
+            }
+        );
 
-    // if bidding, update function
-        //read items from DB
-        //use inquire to show list in order for user to select on item to bid
-        //after deciding, inquire on bid
-        //update highest bid column for specific item
+        console.log(query.sql);
+    }
+
+    function readProducts() {
+        console.log("Selecting all products..\n");
+        connection.query("SELECT * FROM auctions", function (err, res) {
+            if (err) throw err;
+            console.log(res);
+            connection.end();
+        });
+    };
+    
+// if bidding, update function
+//read items from DB
+//use inquire to show list in order for user to select on item to bid
+//after deciding, inquire on bid
+//update highest bid column for specific item
 
 // return message: "Your item has been posted!"
 
-            //or
+//or
 
 // return message:  "You are the highest bidder!" or "Sorry, bro, try again!"
-
-
