49abff1d425cde93fb68a1828b34e3c950306a58
RomeoKilo125 <romeokilo125@gmail.com>
2018-05-27 10:53:34 -0400

    added inquiry for new items

--- greatBay.js
+++ greatBay.js
@@ -3,71 +3,86 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: process.env.password,
-    database: "greatBay_DB"
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: process.env.password,
+  database: "greatBay_DB"
 });
 
-connection.connect(function (err) {
-    if (err) throw err;
-    //TESTING CONNECTION
-    //console.log("Connected as ID " + connection.threadId)
-    //connection.query("SELECT * FROM auctions", function (err, res) {
-    //console.log(res)
-    //createProduct();
-    startApp();
+connection.connect(function(err) {
+  if (err) throw err;
+  //TESTING CONNECTION
+  //console.log("Connected as ID " + connection.threadId)
+  //connection.query("SELECT * FROM auctions", function (err, res) {
+  //console.log(res)
+  //createProduct();
+  startApp();
 });
 // use inquirer to ask user for post or bid
 function startApp() {
-    inquirer
-        .prompt({
-            name: "postOrBid",
-            type: "rawlist",
-            message: "Would you like to [POST] an auction or [BID] on an auction?",
-            choices: ["POST", "BID"]
-        })
-        // once user has decided:
-        .then(function(answer) {
-    // if posting, create function
-            if (answer.postOrBid.toUpperCase() === "POST") {
-                postAuction();
-            }
-            else {
-                bidAuction();
-            }
-        });
-    }
-    // inquire for: item name, category, starting bid
-    //then create function to add item to DB
-    function postAuction() {
-        console.log("Inserting a new bid...\n");
-        var query = connection.query(
-            "INSERT INTO auctions SET ?", {
-                item_name: "Nintendo Switch",
-                category: "Video Games",
-                starting_bid: 0,
-                highest_bid: 0
-            },
-            function (err, res) {
-                console.log(res.affectedRows + " product inserted!\n");
-                readProducts();
-            }
-        );
+  inquirer
+    .prompt({
+      name: "postOrBid",
+      type: "rawlist",
+      message: "Would you like to [POST] an auction or [BID] on an auction?",
+      choices: ["POST", "BID"]
+    })
+    // once user has decided:
+    .then(function(answer) {
+      // if posting, create function
+      if (answer.postOrBid.toUpperCase() === "POST") {
+        postAuction();
+      } else {
+        readProducts();
+      }
+    });
+}
+// inquire for: item name, category, starting bid
+//then create function to add item to DB
+function postAuction() {
+  inquirer
+    .prompt([{
+      name: "newItem",
+      type: "input",
+      message: "What is the name of your item?"
+    }, {
+      name: "category",
+      type: "rawlist",
+      message: "What category does your item fit into?",
+      choices: ["Video Games", "Sports Equipment", "Books", "Cars"]
+    }, {
+      name: "startBid",
+      type: "input",
+      message: "What is the least amount you will accept for your item?"
+    }]).then(function(answers) {
+      console.log("Inserting new item...\n");
+      var query = connection.query(
+        "INSERT INTO auctions SET ?", {
+          item_name: answers.newItem,
+          category: answers.category,
+          starting_bid: answers.startBid,
+          highest_bid: 0
+        },
+        function(err, res) {
+          console.log(query.sql);
+          console.log(res.affectedRows + " product inserted!\n");
+          readProducts();
+        }
+      );
+    });
 
-        console.log(query.sql);
-    }
+}
 
-    function readProducts() {
-        console.log("Selecting all products..\n");
-        connection.query("SELECT * FROM auctions", function (err, res) {
-            if (err) throw err;
-            console.log(res);
-            connection.end();
-        });
-    };
-    
+function readProducts() {
+  console.log("Selecting all products..\n");
+  connection.query("SELECT * FROM auctions", function(err, res) {
+    if (err) throw err;
+    console.log(res);
+    connection.end();
+  });
+};
+
 // if bidding, update function
 //read items from DB
 //use inquire to show list in order for user to select on item to bid
